f33bb677145d3cf1f129316c81c3d4f80022a76f
Octavian Susnea <octavian@sneakersgames.com>
2023-11-30 18:22:05 +0100

    changed port to 3000

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ fastify.post('/return', async (request, reply) => {
 // Start server
 const start = async () => {
     try {
-        await fastify.listen(3003);
+        await fastify.listen(3000);
         fastify.log.info(`server listening on ${fastify.server.address().port}`);
     } catch (err) {
         fastify.log.error(err);
