e8af375ceff7f2e09fdfacf3b8bf5a0c3af1a575
Octavian Susnea <octavian@sneakersgames.com>
2023-11-30 19:54:04 +0100

    now using express

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+const port = process.env.PORT || 3000;
+var jsonParser = bodyParser.json()
+
+const Redis = require('ioredis');
+// Configure Redis client
+const redisUrl = process.env.REDIS_URL || '0.0.0.0:6379';
+//default:bigredisbigresults23@redistack.fanarena.com:6379
+const redis = new Redis(`redis://${redisUrl}`);
+
+app.post('/activate', jsonParser, async (request, res) => {
+  //TODO validate auth header
+  console.log(request.body);
+  const eventId = "01";
+  const score = Date.now();
+
+  //TODO check if array of EPC's and insert multiple EPC one by one
+  const data = request.body;
+
+  //TODO Change to stream to be able to read all scans for a POSID in a certain range 
+  await redis.zadd(`SCAN:${eventId}:${data.POSID}`, score, JSON.stringify(data));
+
+  await redis.hset(`PACKAGE:${eventId}:${data.EPC}`, {
+      EPC: data.EPC,
+      first_seen: data.first_seen,
+      last_seen: data.last_seen,
+      count: data.count,
+      POSID: data.POSID,
+      eventId: eventId,
+      status: 'scanned'
+  }); // JSON.stringify(data));
+
+  // await redis.xadd(`SCAN:${eventId}:${data.POSID}`, '*', 'EPC', data.EPC, 'first_seen', data.first_seen, 'last_seen', data.last_seen, 'count', data.count);
+
+  res.send({ status: 'SUCCESS', message: 'Data saved to Redis' });
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
--- server.js
+++ server.js
@@ -1,41 +1,4 @@
-// // POST /activate route
-// fastify.post('/activate', async (request, reply) => {
-//     //TODO validate auth header
-//     console.log(request.body);
-//     const eventId = "01";
-//     const score = Date.now();
-
-//     //TODO check if array of EPC's and insert multiple EPC one by one
-//     const data = request.body;
-
-//     //TODO Change to stream to be able to read all scans for a POSID in a certain range 
-//     await redis.zadd(`SCAN:${eventId}:${data.POSID}`, score, JSON.stringify(data));
-
-//     await redis.hset(`PACKAGE:${eventId}:${data.EPC}`, {
-//         EPC: data.EPC,
-//         first_seen: data.first_seen,
-//         last_seen: data.last_seen,
-//         count: data.count,
-//         POSID: data.POSID,
-//         eventId: eventId,
-//         status: 'scanned'
-//     }); // JSON.stringify(data));
-
-//     // await redis.xadd(`SCAN:${eventId}:${data.POSID}`, '*', 'EPC', data.EPC, 'first_seen', data.first_seen, 'last_seen', data.last_seen, 'count', data.count);
-
-//     return { status: 'success', message: 'Data saved to Redis' };
-// });
-
-// // POST /return route
-// fastify.post('/return', async (request, reply) => {
-//     const { key } = request.body;
-//     const data = await redis.hgetall(key);
-
-//     //TODO logica voor teruggave
-//     return { status: 'success', message: data };
-// });
-
-const fastify = require('fastify')({ logger: true })
+const fastify = require('fastify')({ logger: true });
 const Redis = require('ioredis');
 
 // Configure Redis client
@@ -44,42 +7,55 @@ const port = process.env.PORT || 3000;
 //default:bigredisbigresults23@redistack.fanarena.com:6379
 const redis = new Redis(`redis://${redisUrl}`);
 
-fastify.route({
-  method: 'GET',
-  url: '/',
-  schema: {
-    // request needs to have a querystring with a `name` parameter
-    querystring: {
-      type: 'object',
-      properties: {
-          name: { type: 'string'}
-      },
-      required: [],
-    },
-    // the response needs to be an object with an `hello` property of type 'string'
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          hello: { type: 'string' }
-        }
-      }
+// POST /activate route
+fastify.post('/activate', async (request, reply) => {
+    //TODO validate auth header
+    console.log(request.body);
+    const eventId = "01";
+    const score = Date.now();
+
+    //TODO check if array of EPC's and insert multiple EPC one by one
+    const data = request.body;
+
+    //TODO Change to stream to be able to read all scans for a POSID in a certain range 
+    await redis.zadd(`SCAN:${eventId}:${data.POSID}`, score, JSON.stringify(data));
+
+    await redis.hset(`PACKAGE:${eventId}:${data.EPC}`, {
+        EPC: data.EPC,
+        first_seen: data.first_seen,
+        last_seen: data.last_seen,
+        count: data.count,
+        POSID: data.POSID,
+        eventId: eventId,
+        status: 'scanned'
+    }); // JSON.stringify(data));
+
+    // await redis.xadd(`SCAN:${eventId}:${data.POSID}`, '*', 'EPC', data.EPC, 'first_seen', data.first_seen, 'last_seen', data.last_seen, 'count', data.count);
+
+    return { status: 'success', message: 'Data saved to Redis' };
+});
+
+// POST /return route
+fastify.post('/return', async (request, reply) => {
+    const { key } = request.body;
+    const data = await redis.hgetall(key);
+
+    //TODO logica voor teruggave
+    return { status: 'success', message: data };
+});
+
+fastify.get('/', async (request, reply) => {
+    return { hello: 'world' };
+});
+
+// Start server
+const start = async () => {
+    try {
+        await fastify.listen(port);
+        fastify.log.info(`server listening on ${fastify.server.address().port}`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
     }
-  },
-  // this function is executed for every request before the handler is executed
-  preHandler: (request, reply, done) => {
-    // E.g. check authentication
-    done()
-  },
-  handler: (request, reply) => {
-    reply.send({ hello: 'world' })
-  }
-})
-
-fastify.listen({ port: port }, (err) => {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-})
-
+};
+start();
