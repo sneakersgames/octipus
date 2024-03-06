c1822e06a2f8d71aae4329c17abde7ecb866efa5
Octavian Susnea <octavian@sneakersgames.com>
2024-03-06 21:27:26 +0100

    webhook endpoint

--- app.js
+++ app.js
@@ -14,6 +14,35 @@ const redis = new Redis(`redis://${redisUrl}`);
 const eventId = "01";
 const score = Date.now();
 
+app.post('/webhooks/:eventId', jsonParser, async (request, res) => {
+  console.log(`Webhook ${eventId} Request \n ----`)
+    //TODO validate auth header
+  console.log('headers', request.headers);
+  console.log('body', request.body);
+    
+  // const data = Array.isArray(request.body) ? request.body : [request.body];
+
+  // data.forEach(async (epc) => {
+  //   //TODO Change to stream to be able to read all scans for a POSID in a certain range 
+  //   await redis.zadd(`SCAN:${eventId}:${epc.POSID}`, score, JSON.stringify(epc));
+  //   await redis.hset(`PACKAGE:${eventId}:${epc.EPC}`, {
+  //     EPC: epc.EPC,
+  //     first_seen: epc.first_seen,
+  //     last_seen: epc.last_seen,
+  //     count: epc.count,
+  //     POSID: epc.POSID,
+  //     eventId: eventId,
+  //     status: 'scanned'
+  //   }); // JSON.stringify(data));
+
+  //   // await redis.xadd(`SCAN:${eventId}:${data.POSID}`, '*', 'EPC', data.EPC, 'first_seen', data.first_seen, 'last_seen', data.last_seen, 'count', data.count);
+
+  //   // TODO send amqp message to try to match last sales (EPC) of this POSID 
+  // });
+
+  res.send({ status: 'SUCCESS', message: 'Data saved to Redis' });
+});
+
 app.post('/activate', jsonParser, async (request, res) => {
   console.log('New Request \n ----')
     //TODO validate auth header
