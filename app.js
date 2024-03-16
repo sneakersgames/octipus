820ece215851f157888bbfdde0df14591be3c09a
Louis Mylle <louis@optimize-it.be>
2024-03-16 15:52:02 +0100

    Undo last standing change yesterday.

--- app.js
+++ app.js
@@ -193,7 +193,7 @@ app.post('/activate', jsonParser, async (request, res) => {
             console.log((`HISTORY:${epc.EPC}`, `${score}-${index}`, 'info', `${eventId} SALE at ${request.body.POSID} with ${epc.count} counts. UTC ${score}, First seen ${epc.first_seen}, Last seen ${epc.last_seen}.`))
             const xaddHISTORY = await redis.xadd(
               `HISTORY:${epc.EPC}`, 
-              `*`, 
+              `${score}-${index}`, 
               'info',
               `${eventId} SALE at ${request.body.POSID} with ${epc.count} counts. UTC ${score}, First seen ${epc.first_seen}, Last seen ${epc.last_seen}.`
             );
@@ -272,7 +272,7 @@ app.post('/activate', jsonParser, async (request, res) => {
             console.log(`HISTORY:${epc.EPC}`, `${score}-${index}`, 'info', `${eventId} RETURN at ${request.body.POSID} with ${epc.count} counts. UTC ${score}, First seen ${epc.first_seen}, Last seen ${epc.last_seen}.`)
             const xaddHISTORY = await redis.xadd(
               `HISTORY:${epc.EPC}`, 
-              `*`, 
+              `${score}-${index}`, 
               'info',
               `${eventId} RETURN at ${request.body.POSID} with ${epc.count} counts. UTC ${score}, First seen ${epc.first_seen}, Last seen ${epc.last_seen}.`
             );
